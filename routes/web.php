42fe816f666a371466fb6b044664e736ef63412c
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-09-25 13:01:17 +0200

    Sistemata rotta principale, creati migration, model e seeder di Technologies

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use App\Http\Controllers\Admin\TypeController;
 |
 */
 
+Route::get('/', [PageController::class, 'index'])->name('home');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
